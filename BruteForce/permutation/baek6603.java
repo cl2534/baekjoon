a2a7823eebed4c5de403e69c9762daa7751766b1
github username <chang.lim92@gmail.com>
2020-07-11 22:51:07 +0900

    perm

---
+++ BruteForce/permutation/baek6603.java
@@ -0,0 +1,92 @@
+/*
+독일 로또는 {1, 2, ..., 49}에서 수 6개를 고른다.
+
+로또 번호를 선택하는데 사용되는 가장 유명한 전략은 49가지 수 중 k(k>6)개의 수를 골라 집합 S를 만든 다음 그 수만 가지고 번호를 선택하는 것이다.
+
+예를 들어, k=8, S={1,2,3,5,8,13,21,34}인 경우 이 집합 S에서 수를 고를 수 있는 경우의 수는 총 28가지이다. 
+([1,2,3,5,8,13], [1,2,3,5,8,21], [1,2,3,5,8,34], [1,2,3,5,13,21], ..., [3,5,8,13,21,34])
+
+집합 S와 k가 주어졌을 때, 수를 고르는 모든 방법을 구하는 프로그램을 작성하시오.
+*/
+import java.util.*;
+public class baek6603 {
+    static boolean next_permutgetion(int[] a) {
+        int i = a.length-1;
+        while (i > 0 && a[i-1] >= a[i]) {
+            i -= 1;
+        }
+
+        if (i <= 0) {
+            return false;
+        }
+
+        int j = a.length-1;
+        while (a[j] <= a[i-1]) {
+            j -= 1;
+        }
+
+        int temp = a[i-1];
+        a[i-1] = a[j];
+        a[j] = temp;
+
+        j = a.length-1;
+        while (i < j) {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            int[] a = new int[n];
+            for (int i=0; i<n; i++) {
+                a[i] = sc.nextInt();
+            }
+            int[] d = new int[n];
+            for (int i=0; i<n; i++) {
+                if (i < n-6) d[i] = 0;
+                else d[i] = 1;
+            }
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            do {
+                ArrayList<Integer> cur = new ArrayList<>();
+                for (int i=0; i<n; i++) {
+                    if (d[i] == 1) {
+                        cur.add(a[i]);
+                    }
+                }
+                ans.add(cur);
+            } while (next_permutgetion(d));
+            Collections.sort(ans, new Comparator<ArrayList<Integer>>() {
+                public int compare(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+                    int n = l1.size();
+                    int m = l2.size();
+                    int i = 0;
+                    while (i < n && i < m) {
+                        int t1 = l1.get(i);
+                        int t2 = l2.get(i);
+                        if (t1 < t2) return -1;
+                        else if (t1 > t2) return 1;
+                        i += 1;
+                    }
+                    if (i == n && i != m) return -1;
+                    else if (i != n && i == m) return 1;
+                    return 0;
+                }
+            });
+            for (ArrayList<Integer> v : ans) {
+                for (int x : v) {
+                    System.out.print(x + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}

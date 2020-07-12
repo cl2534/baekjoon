1355e6113e45fdd044aa7e7093aabd4a06702c5a
github username <chang.lim92@gmail.com>
2020-07-12 21:32:22 +0900

    recur practice

---
+++ BruteForce/recursion/baek6603.java
@@ -0,0 +1,41 @@
+/* 로또 
+독일 로또는 {1, 2, ..., 49}에서 수 6개를 고른다.
+
+로또 번호를 선택하는데 사용되는 가장 유명한 전략은 49가지 수 중 k(k>6)개의 수를 골라 집합 S를 만든 다음 그 수만 가지고 번호를 선택하는 것이다.
+
+예를 들어, k=8, S={1,2,3,5,8,13,21,34}인 경우 이 집합 S에서 수를 고를 수 있는 경우의 수는 총 28가지이다. 
+([1,2,3,5,8,13], [1,2,3,5,8,21], [1,2,3,5,8,34], [1,2,3,5,13,21], ..., [3,5,8,13,21,34])
+
+집합 S와 k가 주어졌을 때, 수를 고르는 모든 방법을 구하는 프로그램을 작성하시오.
+*/
+public class baek6603 {
+    static ArrayList<Integer> lotto = new ArrayList<>();
+    static void solve(int[] a, int index, int cnt) {
+        if (cnt == 6) {
+            for (int num : lotto) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+            return;
+        }
+        int n = a.length;
+        if (n == index) return;
+        lotto.add(a[index]);
+        solve(a, index+1, cnt+1);
+        lotto.remove(lotto.size()-1);
+        solve(a, index+1, cnt);
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int n = sc.nextInt();
+            if (n == 0) break;
+            int[] a = new int[n];
+            for (int i=0; i<n; i++) {
+                a[i] = sc.nextInt();
+            }
+            solve(a, 0, 0);
+            System.out.println();
+        }
+    }
+}
